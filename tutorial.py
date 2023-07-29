22017508e488f548c8c57cbf346d0b8606c4ba03
test-tenant23 <136111392+test-tenant23@users.noreply.github.com>
2023-07-29 05:09:06 -0700

    Topics that will be covered in tutorial

---
+++ tutorial.py
@@ -0,0 +1,15 @@
+# Following https://wiki.python.org/moin/SimplePrograms#:~:text=The%20examples%20below%20will%20increase%20in%20number%20of,For%20loop%2C%20built-in%20enumerate%20function%2C%20new%20style%20formatting
+
+# Output
+
+# Input and Assignment
+
+# If statements
+
+# For loops
+
+# While Loops
+
+# Functions
+
+# Dictionaries
